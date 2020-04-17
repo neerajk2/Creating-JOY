dce8d19de62d2cc72b1f05cceab221df6647be29
Shravan Vemula <shravan17298@gmail.com>
2020-04-17 17:49:16 +0530

    basic code setup

---
+++ creating-joy/src/main/java/com/project/creatingjoy/CreatingJoyApplication.java
@@ -0,0 +1,13 @@
+package com.project.creatingjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatingJoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatingJoyApplication.class, args);
+	}
+
+}
