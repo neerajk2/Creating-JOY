04d57951c29c0124af2a06d1770fb102d2d79023
Shravan Vemula <shravan17298@gmail.com>
2020-04-17 17:49:16 +0530

    basic code setup

---
+++ creating-joy/src/test/java/com/project/creatingjoy/CreatingJoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.creatingjoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatingJoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
